3779538a103ee7154464ad009bb2c0dd1cff4345
josephOri <ori100no@gmail.com>
2025-12-09 14:04:21 +0200

    dockerized application

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,17 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@repo/shared': path.resolve(__dirname, '../../packages/shared/src/index.ts'),
     },
   },
 });
