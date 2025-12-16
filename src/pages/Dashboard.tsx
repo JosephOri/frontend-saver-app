b7ddf0948547c15c8c06e89e0693bfe51055919a
josephOri <ori100no@gmail.com>
2025-12-16 16:41:50 +0200

    only my ui

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,10 +5,6 @@ import {
   AddMemberDialog,
   Loading,
 } from '@/components';
-import { ChartAreaInteractive } from '@/components/chart-area-interactive';
-import { DataTable } from '@/components/data-table';
-import { SectionCards } from '@/components/section-cards';
-import data from '../app/dashboard/data.json';
 
 const Dashboard = () => {
   const { data: user, isLoading: isUserLoading } = useCurrentUser();
@@ -25,18 +21,6 @@ const Dashboard = () => {
       <AddMemberDialog />
 
       <FinancialInfo orders={orders || []} />
-
-      <div className="flex flex-1 flex-col">
-        <div className="@container/main flex flex-1 flex-col gap-2">
-          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <SectionCards />
-            <div className="px-4 lg:px-6">
-              <ChartAreaInteractive />
-            </div>
-            <DataTable data={data} />
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
