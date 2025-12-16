aad8a982a826e42338a97a402c113dd67b9ac5b0
josephOri <ori100no@gmail.com>
2025-12-16 13:02:37 +0200

    added typings and added dialog to dashboard

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,4 @@
 export * from './urls';
+export const localStorageKeys = {
+  NOTIFICATIONS: 'notifications',
+} as const;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,28 @@
 import { useCurrentUser, useFinancialOrders } from '@/hooks';
-import { CreateFinancialOrderDialogForm, FinancialInfo, LogoutButton } from '@/components';
+import {
+  CreateFinancialOrderDialogForm,
+  FinancialInfo,
+  LogoutButton,
+  AddMemberDialog,
+} from '@/components';
+import Loading from '@/components/common/Loading';
 
 const Dashboard = () => {
   const { data: user, isLoading: isUserLoading } = useCurrentUser();
   const { data: orders, isLoading: isOrdersLoading } = useFinancialOrders();
 
-  if (isUserLoading || isOrdersLoading) return <div>Loading...</div>;
+  if (isUserLoading || isOrdersLoading) return <Loading />;
   return (
-    <div className="p-8 space-y-8">
-      <div className="flex flex-col justify-between items-center gap-5">
+    <div className="space-y-8 p-8">
+      <div className="flex flex-col items-center justify-between gap-5">
         <h1 className="text-3xl font-bold">Welcome back, {user?.name}</h1>
         <LogoutButton />
 
         <CreateFinancialOrderDialogForm />
 
         <FinancialInfo orders={orders || []} />
+
+        <AddMemberDialog />
       </div>
     </div>
   );
