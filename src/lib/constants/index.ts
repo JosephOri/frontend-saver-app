42943f6f5e13ebaecb0e940255dedd9865854056
josephOri <ori100no@gmail.com>
2025-12-28 14:30:49 +0200

    added query keys

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -4,3 +4,12 @@ export * from './routes';
 export const localStorageKeys = {
   NOTIFICATIONS: 'notifications',
 } as const;
+
+export const queryKeys = {
+  CATEGORIES: 'categories',
+  TRANSACTIONS: 'transactions',
+  RECURRING_TRANSACTIONS: 'recurringTransactions',
+  DATE_TRANSACTION: 'dateTransaction',
+  HOUSEHOLD: 'household',
+  CURRENT_USER: 'currentUser',
+} as const;
