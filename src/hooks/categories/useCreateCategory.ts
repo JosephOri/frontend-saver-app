6dd3ed9769aded44e9af2d3a948823d1083197f4
josephOri <ori100no@gmail.com>
2026-01-04 15:00:32 +0200

    modified hook

--- src/hooks/categories/useCreateCategory.ts
+++ src/hooks/categories/useCreateCategory.ts
@@ -1,19 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { apiClient, queryKeys, urlSuffixes } from '@/lib';
 import type { Category } from '@/typings/category.type';
-import { useCurrentUser } from '../auth';
 
 type CreateCategoryData = Pick<Category, 'value' | 'type'>;
 
 export const useCreateCategory = () => {
   const queryClient = useQueryClient();
-  const { data: user } = useCurrentUser();
-
   return useMutation({
     mutationFn: async (data: CreateCategoryData) => {
       const { data: newCategory } = await apiClient.post<Category>(
         urlSuffixes.CATEGORIES,
-        { ...data, userId: user?.id },
+        data,
       );
       return newCategory;
     },
